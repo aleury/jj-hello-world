0cbceeac3f8dd8cdde486239fafd39571d6e5323
Adam Eury <euryadam@gmail.com>
2025-08-31 19:39:38 -0400

    add a comment to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-/// A "Hello, world!" program.
-///
-/// This is the best implementation of this program to ever exist!
+//! A "Hello, world!" program.
+//!
+//! This is the best implementation of this program to ever exist!
+
+// add documentation for main
 fn main() {
     print("Hello, world!");
 }
